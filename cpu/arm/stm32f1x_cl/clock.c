68be24e34e3ad3f7997c7279c57fd44669cad942
Karl Palsson <karlp@tweak.net.au>
2013-04-29 07:41:45 +0000

    [stm32-ocm3] Expand clock module implementation
    In certain cases, delaying may be off by up to an entire uSec, when
    the systick counter is about to wrap.  The implementation here is not the
    most precise, with perfectly counted assembly loops, but it's clear and
    functional.  You shouldn't be using these low level routines for anything
    critical anyway.

--- cpu/arm/stm32f1x_cl/clock.c
+++ cpu/arm/stm32f1x_cl/clock.c
@@ -1,7 +1,13 @@
+/**
+ * \file
+ *         libopencm3 implementation of the contiki "clock" module
+ */
+
+
 #include <sys/clock.h>
 #include <sys/cc.h>
+#include <sys/energest.h>
 #include <sys/etimer.h>
-#include <debug-uart.h>
 
 #include <libopencm3/cm3/systick.h>
 
@@ -9,70 +15,92 @@ static volatile clock_time_t current_clock = 0;
 static volatile unsigned long current_seconds = 0;
 static unsigned int second_countdown = CLOCK_SECOND;
 
-void sys_tick_handler(void) __attribute__ ((interrupt));
+/* Systick at AHB_SPEED/8 Mhz, this many ticks per usec */
+#define CLOCK_MICROSECOND_SYSTICK ((AHB_SPEED/8)/1000000)
+
+void sys_tick_handler(void) __attribute__((interrupt));
 
 void
 sys_tick_handler(void)
 {
-  current_clock++;
-  if(etimer_pending() && etimer_next_expiration_time() <= current_clock) {
-    etimer_request_poll();
-    /* printf("%d,%d\n", clock_time(),etimer_next_expiration_time       ()); */
-  }
+	ENERGEST_ON(ENERGEST_TYPE_IRQ);
+	current_clock++;
+	if (etimer_pending() && etimer_next_expiration_time() <= current_clock) {
+		etimer_request_poll();
+	}
 
-  if(--second_countdown == 0) {
-    current_seconds++;
-    second_countdown = CLOCK_SECOND;
-  }
+	if (--second_countdown == 0) {
+		current_seconds++;
+		second_countdown = CLOCK_SECOND;
+	}
+	ENERGEST_OFF(ENERGEST_TYPE_IRQ);
 }
-
 
 void
 clock_init()
 {
-  systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
-
-  /*72mhz / 8 / 1000 */
-  systick_set_reload(MCK / 8 / CLOCK_SECOND);
-
-  systick_interrupt_enable();
-  systick_counter_enable();
+	systick_set_clocksource(STK_CTRL_CLKSOURCE_AHB_DIV8);
+	systick_set_reload(AHB_SPEED / 8 / CLOCK_SECOND);
+	systick_interrupt_enable();
+	systick_counter_enable();
 }
 
 clock_time_t
 clock_time(void)
 {
-  return current_clock;
+	return current_clock;
 }
 
 unsigned long
 clock_seconds(void)
 {
-  return current_seconds;
+	return current_seconds;
 }
 
-/* TODO: This code needs to be evaluated for the stm32f107 and
- * implemented
+void
+clock_set_seconds(unsigned long sec)
+{
+	current_seconds = sec;
+}
+
+void
+clock_wait(clock_time_t i)
+{
+	clock_time_t start;
+
+	start = clock_time();
+	while (clock_time() - start < i);
+}
+
+/**
+ * \brief      Historically 2.83 uSecs, this implementation is in 3uSec ticks
+ * \param t    As per clock.h
+ * \return     void
  */
-#if 0
-/* The inner loop takes 4 cycles. The outer 5+SPIN_COUNT*4. */
-
-#define SPIN_TIME 2             /* us */
-#define SPIN_COUNT (((MCK*SPIN_TIME/1000000)-5)/4)
-
-#ifndef __MAKING_DEPS__
-
 void
 clock_delay(unsigned int t)
 {
-#ifdef __THUMBEL__
-  asm
-    volatile
-    ("1: mov r1,%2\n2:\tsub r1,#1\n\tbne 2b\n\tsub %0,#1\n\tbne 1b\n":"=l"
-     (t):"0"(t), "l"(SPIN_COUNT));
-#else
-#error Must be compiled in thumb mode
-#endif
+	clock_delay_usec(3 * t);
 }
-#endif
-#endif /* __MAKING_DEPS__ */
+
+static
+void inner_delay_usec_one(void)
+{
+	u32 before = STK_VAL;
+	u32 diff = before - CLOCK_MICROSECOND_SYSTICK;
+	if (diff > STK_LOAD) {
+		// TODO -handle wrapping
+	} else {
+		while (STK_VAL > (diff)) {
+			;
+		}
+	}
+}
+
+void
+clock_delay_usec(uint16_t dt)
+{
+	while (dt--) {
+		inner_delay_usec_one();
+	}
+}
